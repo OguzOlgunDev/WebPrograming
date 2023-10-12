fb9446979751490be939fbcdabae68ed452ded29
Oğuz Olgun <86737251+OguzOlgunDev@users.noreply.github.com>
2023-10-12 21:52:57 +0300

    Add files via upload

---
+++ Drum_Kit/index.js
@@ -0,0 +1,97 @@
+
+
+
+var numberofbtn = document.querySelectorAll(".drum").length;
+
+
+for(var i = 0; i<numberofbtn ;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+var innerHtml = this.innerHTML;
+
+makeSound(innerHtml)
+clickAnimation(innerHtml)
+
+
+});
+}
+
+
+
+
+document.addEventListener("keydown", function(event){
+
+makeSound(event.key)
+clickAnimation(event.key)
+
+});
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+
+     var audio = new Audio("sounds/tom-1.mp3");
+     audio.play();
+
+    break;
+
+    case "a":
+
+    var audio = new Audio("sounds/tom-2.mp3");
+     audio.play();
+
+    break;
+
+    case "s":
+
+    var audio = new Audio("sounds/tom-3.mp3");
+     audio.play();
+
+    break;
+
+    case "d":
+
+     var audio = new Audio("sounds/tom-4.mp3");
+     audio.play();
+
+    break;
+
+    case "j":
+
+     var audio = new Audio("sounds/crash.mp3");
+     audio.play();
+
+    break;
+
+    case "k":
+
+     var audio = new Audio("sounds/kick-bass.mp3");
+     audio.play();
+
+    break;
+
+    case "l":
+
+     var audio = new Audio("sounds/snare.mp3");
+     audio.play();
+
+    break;
+    default:  console.log(innerHTML)
+
+
+}};
+
+
+function clickAnimation(currentkey){
+
+var clicked = document.querySelector("."+currentkey)
+clicked.classList.add("pressed")
+
+setTimeout(function(){
+
+ clicked.classList.remove("pressed");
+},100);
+
+}
